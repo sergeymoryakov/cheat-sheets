8b2f6d87dd364322f4b412e1c63e3ced0a10704a
Sergey Moryakov <sergey@moryakov.com>
2023-08-24 21:36:54 +0300

    clean CSs and add new one (js-functions)

--- articles.js
+++ articles.js
@@ -89,22 +89,22 @@ export const articles = [
             <ul>
                 <li>
                     <h2>To display your Git configurations (name and email):</h2>
-                    <pre><code>git config --global --list</code></pre>
+                    <code>git config --global --list</code>
                     <p>This command will list all the global configurations set in Git.</p>
                 </li>
                 <li>
                     <h2>To display your Git username:</h2>
-                    <pre><code>git config user.name</code></pre>
+                    <code>git config user.name</code>
                     <p>Running this command will show your configured username.</p>
                 </li>
                 <li>
                     <h2>To display your Git email:</h2>
-                    <pre><code>git config user.email</code></pre>
+                    <code>git config user.email</code>
                     <p>This command will display the email address you've configured.</p>
                 </li>
                 <li>
                     <h2>To display your SSH key fingerprint:</h2>
-                    <pre><code>ssh-keygen -lf ~/.ssh/id_rsa.pub</code></pre>
+                    <code>ssh-keygen -lf ~/.ssh/id_rsa.pub</code>
                     <p>
                         This command will show the fingerprint of your SSH key. It can be
                         useful for verifying the correct key is associated with your GitHub
@@ -113,7 +113,7 @@ export const articles = [
                 </li>
                 <li>
                     <h2>To check the status of your Git repository:</h2>
-                    <pre><code>git status</code></pre>
+                    <code>git status</code>
                     <p>
                         Running this command within a Git repository will display
                         information about modified, added, and deleted files, as well as the
@@ -122,7 +122,7 @@ export const articles = [
                 </li>
                 <li>
                     <h2>To display the commit history:</h2>
-                    <pre><code>git log</code></pre>
+                    <code>git log</code>
                     <p>
                         This command will show a list of commits in the repository,
                         including the commit message, author, date, and commit hash.
@@ -133,7 +133,7 @@ export const articles = [
                         To display the remote repositories (such as GitHub) configured for
                         your repository:
                     </h2>
-                    <pre><code>git remote -v</code></pre>
+                    <code>git remote -v</code>
                     <p>
                         Running this command will list the remote repositories and their
                         associated URLs.
@@ -141,103 +141,293 @@ export const articles = [
                 </li>
             </ul>
             <p>
-                These commands should give you an overview of your Git configuration and
-                repository status. Additionally, here are a few more commonly used Git
-                commands:
+                Additionally, here are a few more commonly used Git commands:
             </p>
             <ul>
-                <li><code>git add</code>: Stage changes for commit.</li>
                 <li>
-                    <code>git commit</code>: Create a new commit with the staged changes.
+                    <h3>Stage changes for commit:</h3>
+                    <code>git add</code>
                 </li>
-                <li><code>git push</code>: Push commits to a remote repository.</li>
                 <li>
-                    <code>git pull</code>: Fetch and merge changes from a remote repository.
+                    <h3>Create a new commit with the staged changes:</h3>
+                    <code>git commit</code>
                 </li>
-                <li><code>git branch</code>: List, create, or delete branches.</li>
                 <li>
-                    <code>git checkout</code>: Switch branches or restore files from a
-                    commit.
+                    <h3>Push commits to a remote repository:</h3>
+                    <code>git push</code>
                 </li>
-                <li><code>git merge</code>: Merge changes from one branch into another.</li>
-                <li><code>git clone</code>: Create a local copy of a remote repository.</li>
+                <li>
+                    <h3>Fetch and merge changes from a remote repository:</h3>
+                    <code>git pull</code>
+                </li>
+                <li>
+                    <h3>List, create, or delete branches:</h3>
+                    <code>git branch</code>
+                </li>
+                <li>
+                    <h3>Switch branches or restore files from a commit:</h3>
+                    <code>git checkout</code>
+                </li>
+                <li>
+                    <h3>Merge changes from one branch into another:</h3>
+                    <code>git merge</code>
+                </li>
+                <li>
+                    <h3>Create a local copy of a remote repository:</h3>
+                    <code>git clone</code>
+                </li>
             </ul>
             `,
     },
     {
-        title: "JavaScript: Generate Unique ID",
+        title: "JavaScript: Function Types",
         id: "0003",
         tech: "JavaScript",
         body: `
-            <p>
-                There are several approaches you can take to generate unique IDs for your
-                app elements. Here's a breakdown:
-            </p>
-            <p>
-                <br />
-                <strong>Firestore's Built-in IDs:</strong> Firestore itself can generate
-                unique IDs when you create a document without specifying an ID. These IDs
-                are longer than 10 characters but are guaranteed to be unique within a
-                collection.
-            </p>
-            <code>
-                let newDocRef = db.collection('customers').doc(); <br />
-                let uniqueID = newDocRef.id; // This gives you a unique ID generated by
-                Firestore
-            </code>
-            <p>
-                <br />
-                <strong>Shortid Library:</strong> Shortid is a popular library that creates
-                amazingly short non-sequential URL-friendly unique IDs. Perfect for URL
-                slugs, MongoDB, and Redis keys, among others.
-            </p>
-            <p>Installation:</p>
-            <code> npm install shortid</code>
-            <p>Usage:</p>
-            <code>
-                const shortid = require('shortid'); <br />
-                console.log(shortid.generate()); // Produces a short unique ID
-            </code>
-            <p>
-                <br />
-                <strong>Nano ID:</strong> Nano ID is another library that is a
-                size-efficient URL-friendly unique string ID generator.
-            </p>
-            <p>Installation:</p>
-            <code> npm install nanoid </code>
-            <p>Usage:</p>
-            <code>
-                const { nanoid } = require('nanoid'); <br />
-                const id = nanoid(10); // Produces a 10-character unique ID
-            </code>
-            <p>
-                <br />
-                <strong>UUID:</strong> UUID library assists to generate long unique IDs in a
-                format of "a449e3ec-a602-4020-abeb-91f680a0128d", that might work for as a
-                strong password for majority of applications.
-            </p>
-            <p>Installation:</p>
-            <code> npm install uuid </code>
-            <p>Usage:</p>
-            <code>
-                import { v4 as uuidv4 } from "uuid";<br />
-                uuidv4();
-            </code>
-            <p>
-                <br />
-                <strong>Custom Function:</strong> If you want to roll out your own function,
-                here's a simple one:
-            </p>
-            <code>
-                function generateUID(length) { <br />
-                return [...Array(length)].map(() => (~~(Math.random() *
-                36)).toString(36)).join(''); <br />
-                } console.log(generateUID(10)); // Produces a 10-character unique ID
-            </code>
-            <p>
-                This function generates an ID of a given length using numbers and lowercase
-                letters.
-            </p>
-            `,
+        <p>
+            In JavaScript, functions are blocks of code that can be defined and
+            executed when called upon. They are used to perform specific tasks
+            or calculations and are an essential part of the language. The
+            syntax of JavaScript functions consists of three main parts:
+        </p>
+        <h3>Function Declaration:</h3>
+        <p>
+            The most common and traditional way to define a function is using
+            the function declaration. It has the following syntax:
+        </p>
+        <code
+            >function functionName(parameters) {<br />
+            // Function body (code block)<br />
+            // This is where the function&#x27;s logic resides<br />
+            // It performs the desired operations when the function is called<br />
+            return someValue; // Optional, used to return a result<br />
+            }</code
+        >
+        <p>Example:</p>
+        <code
+            >function add(a, b) {<br />
+            return a + b;<br />
+            }</code
+        >
+        <h3>Function Expression:</h3>
+        <p>
+            In JavaScript, functions are also treated as first-class objects,
+            which means they can be assigned to variables. This is known as a
+            function expression. It has the following syntax:
+        </p>
+        <code>
+            const functionName = function(parameters) {<br />
+            // Function body (code block)<br />
+            return someValue; // Optional, used to return a result<br />
+            };
+        </code>
+        <p>Example:</p>
+        <code>
+            const subtract = function(a, b) {<br />
+            return a - b;<br />
+            };<br />
+        </code>
+        <h3>Arrow Function Expression:</h3>
+        <p>
+            Introduced in ECMAScript 6 (ES6), arrow functions provide a more
+            concise syntax for defining functions, especially when the function
+            is short. They also have some differences in how they handle the
+            <strong>'this'</strong> keyword compared to regular functions. The
+            syntax for an arrow function is as follows:
+        </p>
+        <code>
+            const functionName = (parameters) =&gt; {<br />
+            // Function body (code block)<br />
+            return someValue; // Optional, used to return a result<br />
+            };<br />
+        </code>
+        <p>Example:</p>
+        <code>
+            const multiply = (a, b) =&gt; {<br />
+            return a \* b;<br />
+            };<br />
+        </code>
+        <h3>Differences between these three methods:</h3>
+        <strong>Hoisting:</strong>
+        <p>
+            Function declarations are hoisted, meaning you can call them before
+            their actual definition in the code.
+        </p>
+        <p>
+            Function expressions and arrow functions are not hoisted, so you
+            must define them before calling.
+        </p>
+        <strong>'this' context:</strong>
+        <p>
+            In regular function declarations and expressions, the value of
+            <strong>'this'</strong> is dynamically determined based on how the
+            function is called. It can vary depending on the context.
+        </p>
+        <p>
+            In arrow functions, <strong>'this'</strong> is lexically scoped,
+            which means it takes the value of <strong>'this'</strong> from its
+            enclosing function or scope.
+        </p>
+        <strong>Syntax length:</strong>
+        <p>
+            Arrow functions provide a more concise syntax, especially for
+            one-liner functions.
+        </p>
+        <p>
+            Which method to use depends on the situation. Function declarations
+            are generally used for named functions, whereas function expressions
+            and arrow functions are often used for anonymous functions or when
+            you need to pass functions as arguments to other functions (e.g., in
+            higher-order functions).
+        </p>
+        <h3>Differences:</h3>
+        <p>
+            The main difference lies in when you define the function in your
+            code:
+        </p>
+        <h3>Function Declaration:</h3>
+        <p>
+            Function declarations are hoisted, which means they are available
+            throughout the scope they are defined in, even before the actual
+            declaration in the code.
+        </p>
+        <p>
+            You can call a function declared using the function keyword at any
+            point in your code, even before the line where the function is
+            declared.
+        </p>
+        <p>Example:</p>
+        <code>
+            // Calling the &#x27;add&#x27; function before its declaration<br />
+            const result = add(5, 10);<br />
+            console.log(result); // Output: 15<br />
+            <br />
+            // Function declaration<br />
+            function add(a, b) {<br />
+            return a + b;<br />
+            }
+        </code>
+        <h3>Function Expression:</h3>
+        <p>
+            Function expressions are not hoisted, so you need to define them
+            before calling them.
+        </p>
+        <p>
+            If you try to call an expressed function before its definition, you
+            will get an error.
+        </p>
+        <p>Example:</p>
+        <code>
+            // This will cause an error: &quot;TypeError: subtract is not a
+            function&quot;<br />
+            const result = subtract(10, 5);<br />
+            <br />
+            // Function expression<br />
+            const subtract = function(a, b) {<br />
+            return a - b;<br />
+            };
+        </code>
+        <p>
+            To avoid errors with function expressions, make sure you define the
+            function before you attempt to call it.
+        </p>
+        <h3>Arrow Function Expression:</h3>
+        <p>
+            Like function expressions, arrow functions are also not hoisted, so
+            they should be defined before calling.
+        </p>
+        <p>
+            The same rule applies here: define the arrow function before calling
+            it.
+        </p>
+        <p>Example:</p>
+        <code>
+            // This will cause an error: &quot;TypeError: multiply is not a
+            function&quot; <br />
+            const result = multiply(2, 3);<br />
+            // Arrow function expression<br />
+            const multiply = (a, b) =&gt; {<br />
+            return a \* b;<br />
+            };
+        </code>
+        <p>
+            So, the importance of expressing a function before calling it
+            applies to both function expressions and arrow functions, as you
+            need to ensure the function is defined and available in memory at
+            the time of the call.
+        </p>
+        `,
+    },
+    {
+        title: "JavaScript: Generate Unique ID",
+        id: "0004",
+        tech: "JavaScript",
+        body: `
+        <p>
+            There are several approaches you can take to generate unique IDs for
+            your app elements. Here's a breakdown:
+        </p>
+        <h3>Firestore's Built-in IDs:</h3>
+        <p>
+            Firestore itself can generate unique IDs when you create a document
+            without specifying an ID. These IDs are longer than 10 characters
+            but are guaranteed to be unique within a collection.
+        </p>
+        <code>
+            let newDocRef = db.collection('customers').doc(); <br />
+            let uniqueID = newDocRef.id; // This gives you a unique ID generated
+            by Firestore
+        </code>
+        <h3>Shortid Library:</h3>
+        <p>
+            Shortid is a popular library that creates amazingly short
+            non-sequential URL-friendly unique IDs. Perfect for URL slugs,
+            MongoDB, and Redis keys, among others.
+        </p>
+        <p>Installation:</p>
+        <code> npm install shortid</code>
+        <p>Usage:</p>
+        <code>
+            const shortid = require('shortid'); <br />
+            console.log(shortid.generate()); // Produces a short unique ID
+        </code>
+        <h3>Nano ID:</h3>
+        <p>
+            Nano ID is another library that is a size-efficient URL-friendly
+            unique string ID generator.
+        </p>
+        <p>Installation:</p>
+        <code> npm install nanoid </code>
+        <p>Usage:</p>
+        <code>
+            const { nanoid } = require('nanoid'); <br />
+            const id = nanoid(10); // Produces a 10-character unique ID
+        </code>
+        <h3>UUID:</h3>
+        <p>
+            UUID library assists to generate long unique IDs in a format of
+            "a449e3ec-a602-4020-abeb-91f680a0128d", that might work for as a
+            strong password for majority of applications.
+        </p>
+        <p>Installation:</p>
+        <code>npm install uuid</code>
+        <p>Usage:</p>
+        <code>
+            import { v4 as uuidv4 } from "uuid";<br />
+            uuidv4();
+        </code>
+        <h3>Custom Function:</h3>
+        <p>If you want to roll out your own function, here's a simple one:</p>
+        <code>
+            function generateUID(length) { <br />
+            return [...Array(length)].map(() => (~~(Math.random() *
+            36)).toString(36)).join(''); <br />
+            } console.log(generateUID(10)); // Produces a 10-character unique ID
+        </code>
+        <p>
+            This function generates an ID of a given length using numbers and
+            lowercase letters.
+        </p>
+        `,
     },
 ];
--- drafts/_article.js
+++ drafts/_article.js
@@ -1,6 +1,23 @@
-export const article = {
-    tytle: `
-`,
-    body: `
-`,
-};
+export const article = [
+    {
+        title: "",
+        id: "0002",
+        tech: "",
+        body: `
+        `,
+    },
+    {
+        title: "",
+        id: "0002",
+        tech: "",
+        body: `
+        `,
+    },
+    {
+        title: "",
+        id: "0002",
+        tech: "",
+        body: `
+        `,
+    },
+];
